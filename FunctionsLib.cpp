32365d8beed63eef1a67b5bfdeafafc70abb722a
ezequiel <ezedepetris@gmail.com>
2017-03-06 13:39:00 -0300

    add function time to destiny

--- FunctionsLib.cpp
+++ FunctionsLib.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 namespace Elevator{
   int Functions::Add(int a, int b){
-      return a + b;
+		return a + b;
   }
+
+	double TimeToDestiny(int origin_floor, int destiny_floor, double time_sended, double time_now){
+		double avanced_floor = (time_now - time_sended) * 2;
+		double floors_to_destiny = destiny_floor - (origin_floor - avanced_floor);
+		return floors_to_destiny * 2;
+	}
 }
--- FunctionsLib.h
+++ FunctionsLib.h
@@ -1,9 +1,10 @@
 // Functions.h
 
 namespace Elevator{
-  class Functions{
-      public:
-          // Returns a + b
-          static int Add(int a, int b);
+	class Functions{
+		public:
+			// Returns a + b
+			static int Add(int a, int b);
+			static double TimeToDestiny(int origin_floor, int destiny_floor, double time_sended, double time_now);
   };
 }
